55e28927980517c6b9fa962711b58435fc6a65c5
Monalisa Menezes <psimonafmenezes@gmail.com>
2026-05-27 21:18:24 -0300

    Adiciona testes da leitura de cliente

--- src/test/java/com/cliente/hexagonal/adapters/in/controller/mapper/CustomerMapperTest.java
+++ src/test/java/com/cliente/hexagonal/adapters/in/controller/mapper/CustomerMapperTest.java
@@ -1,6 +1,8 @@
 package com.cliente.hexagonal.adapters.in.controller.mapper;
 
 import com.cliente.hexagonal.adapters.in.controller.request.CustomerRequest;
+import com.cliente.hexagonal.adapters.in.controller.response.CustomerResponse;
+import com.cliente.hexagonal.application.core.domain.Address;
 import com.cliente.hexagonal.application.core.domain.Customer;
 import org.junit.jupiter.api.Test;
 import org.mapstruct.factory.Mappers;
@@ -27,5 +29,24 @@ class CustomerMapperTest {
         assertNull(customer.getId());
         assertNull(customer.getAddress());
         assertFalse(customer.getValidCpf());
+    }
+
+    @Test
+    void shouldMapDomainToResponse() {
+        Customer customer = new Customer(
+                "id-1",
+                "Maria",
+                "12345678901",
+                new Address("Rua A", "Fortaleza", "CE"),
+                true);
+
+        CustomerResponse response = customerMapper.toCustomerResponse(customer);
+
+        assertEquals("Maria", response.getName());
+        assertEquals("12345678901", response.getCpf());
+        assertEquals(Boolean.TRUE, response.getIsValidCpf());
+        assertEquals("Rua A", response.getAddress().getStreet());
+        assertEquals("Fortaleza", response.getAddress().getCity());
+        assertEquals("CE", response.getAddress().getState());
     }
 }
