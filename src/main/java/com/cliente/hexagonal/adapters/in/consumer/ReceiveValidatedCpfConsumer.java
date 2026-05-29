554436b9131bebf29a38b8b0cd637b68f79ad8bd
Monalisa Menezes <psimonafmenezes@gmail.com>
2026-05-28 21:48:29 -0300

    Implement Kafka producer and consumer configuration; add customer message handling and validation logic

--- src/main/java/com/cliente/hexagonal/adapters/in/consumer/ReceiveValidatedCpfConsumer.java
+++ src/main/java/com/cliente/hexagonal/adapters/in/consumer/ReceiveValidatedCpfConsumer.java
@@ -1,4 +1,25 @@
-package com.cliente.hexagonal.adapters.in.consumer.message;
+package com.cliente.hexagonal.adapters.in.consumer;
 
+import com.cliente.hexagonal.adapters.in.consumer.mapper.CustomerMessageMapper;
+import com.cliente.hexagonal.adapters.in.consumer.message.CustomerMessage;
+import com.cliente.hexagonal.application.core.domain.Customer;
+import com.cliente.hexagonal.application.ports.in.UpdateCustomerInputPort;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.kafka.annotation.KafkaListener;
+import org.springframework.stereotype.Component;
+
+@Component
 public class ReceiveValidatedCpfConsumer {
+
+    @Autowired
+    private UpdateCustomerInputPort updateCustomerInputPort;
+
+    @Autowired
+    private CustomerMessageMapper customerMessageMapper;
+
+    @KafkaListener(topics = "tp-cpf-validated", groupId = "clientes")
+    public void receive(CustomerMessage customerMessage) {
+        Customer customer = customerMessageMapper.toCustomer(customerMessage);
+        updateCustomerInputPort.update(customer, customerMessage.getZipCode());
+    }
 }
--- src/main/java/com/cliente/hexagonal/adapters/in/consumer/mapper/CustomerMessageMapper.java
+++ src/main/java/com/cliente/hexagonal/adapters/in/consumer/mapper/CustomerMessageMapper.java
@@ -1,4 +1,13 @@
 package com.cliente.hexagonal.adapters.in.consumer.mapper;
 
+import com.cliente.hexagonal.adapters.in.consumer.message.CustomerMessage;
+import com.cliente.hexagonal.application.core.domain.Customer;
+import org.mapstruct.Mapper;
+import org.mapstruct.Mapping;
+
+@Mapper(componentModel = "spring")
 public interface CustomerMessageMapper {
+
+    @Mapping(target = "address", ignore = true)
+    Customer toCustomer(CustomerMessage customerMessage);
 }
